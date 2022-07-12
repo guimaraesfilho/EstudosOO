ac8a01227f8aae5a11c2469137a07930d8224d2a
João Batista Guimarães Filho <guimaraesfilho@gmail.com>
2022-07-12 18:01:15 -0300

    Atualização para os conceitos de composição!

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,35 @@
 public class Principal {
 
     public static void main(String[] args) {
+        Pessoa eu = new Pessoa();
+        eu.nome = "Joao Alberto";
+        eu.cpf = "000.111.222-33";
+        eu.anoNascimento = 1978;
+
+        Pessoa voce = new Pessoa();
+        voce.nome = "Carlos Alberto";
+        voce.cpf = "444.111.555-33";
+        voce.anoNascimento = 1990;
 
         Carro meuCarro = new Carro();
         meuCarro.anoFabricacao = 2021;
         meuCarro.cor = "preto";
         meuCarro.fabricante = "Honda";
         meuCarro.modelo = "HR-V";
+        meuCarro.proprietario = eu;
 
         Carro seuCarro = new Carro();
         seuCarro.anoFabricacao = 2022;
         seuCarro.cor = "Branco";
         seuCarro.fabricante = "BMW";
         seuCarro.modelo = "X6";
+        seuCarro.proprietario = voce;
 
         System.out.println("Meu carro");
         System.out.println("----------");
         System.out.printf("Modelo: %s%n", meuCarro.modelo);
         System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+
 
         System.out.println();
 

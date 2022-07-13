6c7f38f15c2159dbf408d00d5cb9aa5e80cf9e6a
João Batista Guimarães Filho <guimaraesfilho@gmail.com>
2022-07-13 19:16:27 -0300

    Implemtentando métodos menores!

--- src/Carro.java
+++ src/Carro.java
@@ -7,18 +7,34 @@ public class Carro {
     double precoCompra;
     Pessoa proprietario;
 
-    void calcularValorRevenda(){
-        int tempoDeUsoEmAnos = 2022 - anoFabricacao;
+    void imprimirResumoDepreciacao() {
+        double valorRevendaMeuCarro = calcularValorRevenda();
+        int tempoDeUsoMeuCarro = calcularTempoDeUsoEmAnos();
+
+        System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoMeuCarro);
+        System.out.printf("Valor de revenda: %6.2f%n", valorRevendaMeuCarro);
+    }
+
+    double calcularIpva() {
+        return calcularValorRevenda() * 0.04;
+    }
+
+    int calcularTempoDeUsoEmAnos() {
+        return 2022 - anoFabricacao;
+    }
+
+    double calcularValorRevenda() {
+        int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
         int vidaUtilEmAnos = 20;
 
-        double valorRevenda = (precoCompra / vidaUtilEmAnos) * (vidaUtilEmAnos - tempoDeUsoEmAnos);
+        double valorRevenda = (precoCompra / vidaUtilEmAnos)
+                * (vidaUtilEmAnos - tempoDeUsoEmAnos);
 
-        if (valorRevenda < 0){
+        if (valorRevenda < 0) {
             valorRevenda = 0;
         }
 
-        System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoEmAnos);
-        System.out.printf("Valor de revenda: %6.2f%n", valorRevenda);
+        return valorRevenda;
     }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -4,18 +4,18 @@ public class Principal {
 
     public static void main(String[] args) {
         Pessoa eu = new Pessoa();
-        eu.nome = "Joao Alberto";
-        eu.cpf = "000.111.222-33";
-        eu.anoNascimento = 1978;
+        eu.nome = "Thiago Faria";
+        eu.cpf = "111.222.333-44";
+        eu.anoNascimento = 1981;
 
         Pessoa voce = new Pessoa();
-        voce.nome = "Carlos Alberto";
-        voce.cpf = "444.111.555-33";
-        voce.anoNascimento = 1990;
+        voce.nome = "João das Couves";
+        voce.cpf = "999.999.999-99";
+        voce.anoNascimento = 1992;
 
         Carro meuCarro = new Carro();
-        meuCarro.anoFabricacao = 2015;
-        meuCarro.cor = "preto";
+        meuCarro.anoFabricacao = 2021;
+        meuCarro.cor = "Preto";
         meuCarro.fabricante = "Honda";
         meuCarro.modelo = "HR-V";
         meuCarro.precoCompra = 120000;
@@ -26,26 +26,30 @@ public class Principal {
         seuCarro.cor = "Branco";
         seuCarro.fabricante = "BMW";
         seuCarro.modelo = "X6";
-        seuCarro.precoCompra = 160000;
+        seuCarro.precoCompra = 980000;
         seuCarro.proprietario = voce;
 
-        meuCarro.calcularValorRevenda();
-        seuCarro.calcularValorRevenda();
+        meuCarro.imprimirResumoDepreciacao();
+        seuCarro.imprimirResumoDepreciacao();
+
+//        double valorRevendaMeuCarro = meuCarro.calcularValorRevenda();
+//        int tempoDeUsoMeuCarro = meuCarro.calcularTempoDeUsoEmAnos();
+//
+//        System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoMeuCarro);
+//        System.out.printf("Valor de revenda: %6.2f%n", valorRevendaMeuCarro);
 
 //        System.out.println("Meu carro");
-//        System.out.println("----------");
+//        System.out.println("---------");
 //        System.out.printf("Modelo: %s%n", meuCarro.modelo);
-//        System.out.printf("Modelo: %d%n", meuCarro.anoFabricacao);
+//        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
 //        System.out.printf("Proprietário: %s%n", meuCarro.proprietario.nome);
 //
 //        System.out.println();
 //
 //        System.out.println("Seu carro");
-//        System.out.println("----------");
+//        System.out.println("---------");
 //        System.out.printf("Modelo: %s%n", seuCarro.modelo);
-//        System.out.printf("Modelo: %d%n", seuCarro.anoFabricacao);
+//        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
 //        System.out.printf("Proprietário: %s%n", seuCarro.proprietario.nome);
-
-
     }
 }
